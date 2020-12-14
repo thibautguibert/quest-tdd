cba976c54f44c55c7a9ef322ebb6247e7d63a500
Thibaut <thibaut.guibert@edu.escp.eu>
2020-12-14 09:45:19 +0100

    function written

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,11 @@
 const assert = require('assert');
+const capitalizeFirst = require("./capitalizeFirst");
 
+const capitalizeFirstLetters = (string) => {
+    const words = string.split(" ");
+    const capitalizedWords = words.map(word => capitalizeFirst(word));
+    return capitalizedWords.join(" ");
+}
 
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
